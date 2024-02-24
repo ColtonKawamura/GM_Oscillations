1f802350d2f8cfa26350a63cc222523af039c428
Colton Kawamura <48538658+ColtonKawamura@users.noreply.github.com>
2024-02-24 05:58:50 -0500

    Added a "catch block" to keep going if error occurs. Works for probe particle!

--- attenuation_z_probe.m
+++ attenuation_z_probe.m
@@ -10,7 +10,7 @@ data = textread('plotdata_probes_zdisp.txt', '', 'headerlines', 1);
 
 % Extract columns
 step = data(:, 1);
-probe_columns = data(:, 3:end); %changed this from the 2nd column to 3rd because 1st probe breaks corrcoef and is a bottomwall group
+probe_columns = data(:, 2:end); %changed this from the 2nd column to 3rd because 1st probe breaks corrcoef and is a bottomwall group
 
 % Create variables for each probe
 num_probes = size(probe_columns, 2);
@@ -20,48 +20,52 @@ dt = 2.27326038544775e-05;
 driving_frequency = 1;
 
 for probe_number = 1:num_probes
-    % Get the specified probe data
-    probe_data = real(probe_columns(:, probe_number));
+    try
+        % Get the specified probe data
+        probe_data = real(probe_columns(:, probe_number));
 
-    % Find initial oscillation index
-    index_initial_oscillation = find(probe_data > probe_data(1) + 0.5 * (max(probe_data) - probe_data(1)), 1, 'first');
+        % Find initial oscillation index
+        index_initial_oscillation = find(probe_data > probe_data(1) + 0.5 * (max(probe_data) - probe_data(1)), 1, 'first');
 
-    % Prepare data for fitting
-    fit_x = step(index_initial_oscillation:end) * dt;
-    fit_y = probe_data(index_initial_oscillation:end) - probe_data(1);
+        % Prepare data for fitting
+        fit_x = step(index_initial_oscillation:end) * dt;
+        fit_y = probe_data(index_initial_oscillation:end) - probe_data(1);
 
-    % Define function to fit
-    fit_function = @(b, X) b(1) .* sin(2 * pi * driving_frequency * X - b(2));
-    
-    % Define least-squares cost function
-    cost_function = @(b) sum((fit_function(b, fit_x) - fit_y).^2);
+        % Define function to fit
+        fit_function = @(b, X) b(1) .* sin(2 * pi * driving_frequency * X - b(2));
 
-    % Initial guess
-    initial_amplitude = 0.001;
-    initial_phase_offset = 0;
-    initial_guess = [initial_amplitude; initial_phase_offset];
+        % Define least-squares cost function
+        cost_function = @(b) sum((fit_function(b, fit_x) - fit_y).^2);
 
-    % Perform fitting
-    [s, ~, ~] = fminsearch(cost_function, initial_guess);
+        % Initial guess
+        initial_amplitude = 0.001;
+        initial_phase_offset = 0;
+        initial_guess = [initial_amplitude; initial_phase_offset];
 
-    % Calculate correlation coefficient
-    [R, ~] = corrcoef(fit_function(s, fit_x), fit_y);
-    R2 = R(1, 2)^2;
+        % Perform fitting
+        [s, ~, ~] = fminsearch(cost_function, initial_guess);
 
-    % Check if R2 is less than 0.5
-    if R2 < 0.5
-        disp(['R^2 value is less than 0.5 for probe ' num2str(probe_number) ', stopping further processing.']);
-    else
-        % If good, store the vector
-        inital_position_vector = [inital_position_vector, probe_data(1)];
-        amplitude_vector = [amplitude_vector, s(1)];
+        % Calculate correlation coefficient
+        [R, ~] = corrcoef(fit_function(s, fit_x), fit_y);
+        R2 = R(1, 2)^2;
+
+        % Check if R2 is less than 0.5
+        if R2 < 0.5
+            disp(['R^2 value is less than 0.5 for probe ' num2str(probe_number) ', stopping further processing.']);
+        else
+            % If good, store the vector
+            inital_position_vector = [inital_position_vector, probe_data(1)];
+            amplitude_vector = [amplitude_vector, s(1)];
+        end
+    catch
+        disp(['Error occurred for probe ' num2str(probe_number) ', continuing with the next probe.']);
     end
 end
 
 figure;
-    plot(inital_position_vector, amplitude_vector, 'b.')
-    xlabel('Distance');
-    ylabel('Probe Oscilation Amplitude');
-    title('Attentuation of Oscillation in Probes', 'FontSize', 16);
-    legend(cellstr(num2str([varargin{:}]')), 'Location', 'best');
-    grid on;
+plot(inital_position_vector, amplitude_vector, 'b.')
+xlabel('Distance');
+ylabel('Probe Oscillation Amplitude');
+title('Attenuation of Oscillation in Probes', 'FontSize', 16);
+legend(cellstr(num2str([1:num_probes]')), 'Location', 'best'); % Assuming you want probe numbers as legend entries
+grid on;
