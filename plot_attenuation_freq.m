659dfe97b126a7c92e22313f0fcf935de0b43c81
Colton Kawamura <48538658+ColtonKawamura@users.noreply.github.com>
2024-02-27 16:35:11 -0500

    Added other columns

--- plot_attenuation_freq.m
+++ plot_attenuation_freq.m
@@ -4,6 +4,10 @@ data = dlmread('attenuation_data.txt', ',');
 % Extract columns
 frequency = data(:, 1);
 attenuation = data(:, 2);
+kn = data(:, 3);
+kt = data(:, 4);
+gamma_n = data(:, 5);
+gamma_t = data(:, 6);
 
 % Plot frequency vs. gamma_n
 plot(frequency, attenuation, 'o');
