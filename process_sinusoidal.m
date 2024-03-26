78189fb3a06058dcca27d5894c914203b920144f
Colton Kawamura <48538658+ColtonKawamura@users.noreply.github.com>
2024-03-26 15:40:45 -0400

    Added driving amplitude as a variable pulled from LAMMPS outputs

--- process_sinusoidal.m
+++ process_sinusoidal.m
@@ -92,6 +92,9 @@ for i = 1:numel(data_files_info)
             elseif ~isempty(strfind(line, 'dimensionless_p'))
                 dimensionless_p_str = line(strfind(line, 'dimensionless_p=')+16:end);
                 dimensionless_p = str2double(dimensionless_p_str);
+            elseif ~isempty(strfind(line, 'driving_amplitude'))
+                driving_amplitude_str = line(strfind(line, 'driving_amplitude=')+16:end);
+                driving_amplitude = str2double(driving_amplitude_str);
             end
         end
 
@@ -118,7 +121,7 @@ for i = 1:numel(data_files_info)
                 cost_function = @(b) sum((fit_function(b, fit_x) - fit_y).^2);
 
                 % Initial guess
-                initial_amplitude = 0.001;
+                initial_amplitude = driving_amplitude;
                 initial_phase_offset = 0;
                 initial_guess = [initial_amplitude; initial_phase_offset];
 
@@ -212,7 +215,7 @@ for i = 1:numel(data_files_info)
             attenuation = abs(slope);
 
             % Append the new data to the existing data
-            new_data = [driving_frequency, attenuation, kn, kt, gamma_n, gamma_t, dimensionless_p];
+            new_data = [driving_frequency, attenuation, kn, kt, gamma_n, gamma_t, dimensionless_p, driving_amplitude];
             combined_data = [existing_data; new_data];
 
             % Write the combined data to the attenuation file
