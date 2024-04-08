ed97126d3e2e6e53adad5ae7e68ef1e1625c5893
Colton Kawamura <48538658+ColtonKawamura@users.noreply.github.com>
2024-04-08 10:29:09 -0400

    Inital commit change

--- process_fft.m
+++ process_fft.m
@@ -111,49 +111,71 @@ for i = 1:numel(data_files_info)
                 % Get the specified probe data
                 probe_data = real(probe_columns(:, probe_number));
 
-                % Find initial oscillation index when displacement rise above 1/2 max displacement
-                index_initial_oscillation = find(probe_data > probe_data(1) + 0.5 * (max(probe_data) - probe_data(1)), 1, 'first');
+                % Perform fft on the current probe
 
-                % Prepare data for fitting
-                fit_x = step(index_initial_oscillation:end) * dt;
-                fit_y = probe_data(index_initial_oscillation:end) - probe_data(1);
+                average_dt = mean(diff(time));
+                sampling_freq = 1/average_dt;
+                Fn = sampling_freq/2; % Nyquist frequency 
+                number_elements_time = numel(time);
+                centered_data = probe_data-mean(probe_data); %Center the data on zero for mean
+                normalized_fft_data = fft(centered_data)/number_elements_time; 
+                freq_vector = linspace(0, 1, fix(number_elements_time/2)+1)*Fn;
+                index_vector = 1:numel(freq_vector);
 
-                % Define function to fit
-                fit_function = @(b, X) b(1) .* sin(2 * pi * driving_frequency * X - b(2));
+                % Find the dominant frequency and its amplitude
+                %   Need to double it because when signal is centered, power is
+                %   distributed in both positive and negative. double the abs accounts for this
+                [amplitude, idx_max] = max(abs(normalized_fft_data(index_vector)) * 2);
+                dominant_frequency = freq_vector(idx_max);
 
-                % Define least-squares cost function
-                cost_function = @(b) sum((fit_function(b, fit_x) - fit_y).^2);
+                % Find the index of the frequency closest to driving frequency
+                desired_frequency = driving_frequency;
 
-                % Initial guess
-                initial_amplitude =  driving_amplitude;
-                initial_guess = [initial_amplitude; initial_phase_offset];
+                % Find the index of the closest frequency to the desired frequency
+                [~, idx_desired] = min(abs(freq_vector - desired_frequency));
 
-                % Perform fitting
-                [s, ~, ~] = fminsearch(cost_function, initial_guess);
+                % Check if there is a peak around the desired frequency
+                if idx_desired > 1 && idx_desired < numel(freq_vector)
+                    % Calculate the sign of the slope before and after the desired frequency
+                    sign_slope_before = sign(normalized_fft_data(idx_desired) - normalized_fft_data(idx_desired - 1));
+                    sign_slope_after = sign(normalized_fft_data(idx_desired + 1) - normalized_fft_data(idx_desired));
+                    
+                    % Check if the signs of the slopes are different and if the values on both sides are greater than the value at the desired frequency
+                    if sign_slope_before ~= sign_slope_after && normalized_fft_data(idx_desired - 1) < normalized_fft_data(idx_desired) && normalized_fft_data(idx_desired + 1) < normalized_fft_data(idx_desired)
+                        fprintf('Peak found around the driving frequency. Performing sinusoidal fit for phase\n');
 
-                % Update the initial phase offset for the next iteration
-                 initial_phase_offset = s(2);
+                        % Find initial oscillation index when displacement rise above 1/2 max displacement
+                        index_initial_oscillation = find(probe_data > probe_data(1) + 0.5 * (max(probe_data) - probe_data(1)), 1, 'first');
+                        % Prepare data for fitting
+                        fit_x = step(index_initial_oscillation:end) * dt;
+                        fit_y = probe_data(index_initial_oscillation:end) - probe_data(1);
 
-                % Calculate correlation coefficient
-                [R, ~] = corrcoef(fit_function(s, fit_x), fit_y);
-                R2 = R(1, 2)^2;
+                        % Define function to fit
+                        fit_function = @(b, X) b(1) .* sin(2 * pi * driving_frequency * X - b(2));
 
-                % Check if R2 is less than 0.5
-                if R2 < 0.5
-                    % disp(['R^2 value is less than 0.5 for probe ' num2str(probe_number) ', continuing with the next probe.']);
+                        % Define least-squares cost function
+                        cost_function = @(b) sum((fit_function(b, fit_x) - fit_y).^2);
+
+                        % Initial guess
+                        initial_amplitude =  driving_amplitude;
+                        initial_guess = [initial_amplitude; initial_phase_offset];
+
+                        % Perform fitting
+                        [s, ~, ~] = fminsearch(cost_function, initial_guess);
+
+                        % Update the initial phase offset for the next iteration
+                        initial_phase_offset = s(2);
+                        amplitude_vector = [amplitude_vector, amplitude]; % Pulls amplitude from fft calculation
+                        initial_position_vector = [initial_position_vector, probe_data(1)];
+                        phase_vector = [phase_vector, s(2)];
+                        valid_probe_numbers = [valid_probe_numbers, probe_number];
+                    else
+                        fprintf('*** Alert: No peak found around the driving frequency. ***\n');
+                    end
                 else
-                    % If good, store the vector and probe number
-                    initial_position_vector = [initial_position_vector, probe_data(1)];
-                    amplitude_vector = [amplitude_vector, s(1)];
-                    phase_vector = [phase_vector, s(2)];
-                    valid_probe_numbers = [valid_probe_numbers, probe_number];
-                    clear 'R';
-                    clear 'R2';
+                    fprintf('*** Alert: No peak found around the driving frequency. ***\n');
                 end
             catch
-                % disp(['R^2 value is less than 0.5 for probe ' num2str(probe_number) ', continuing with the next probe.']);
-                clear 'R';
-                clear 'R2';
             end
         end
 
@@ -191,15 +213,12 @@ for i = 1:numel(data_files_info)
             semilogy(initial_position_vector, fit_line, 'r-', 'DisplayName', 'Linear Fit');
             xlabel('Distance');
             ylabel('Probe Oscillation Amplitude');
-            title('Linear Fit of Attenuation of Oscillation in Probes', 'FontSize', 16);
+            % title('Linear Fit of Attenuation of Oscillation in Probes', 'FontSize', 16);
+                % Set the title with variables
+            title(sprintf('f=%.2f, k_n=%.2f, gamma_n=%.2f, P=%.2f, alpha=%.2f', driving_frequency, kn, gamma_n, dimensionless_p, slope), 'FontSize', 12);
             legend('show');
             grid on;
 
-
-            % Add equation text to the plot
-            text_location_x = max(initial_position_vector);
-            text_location_y = max(abs(amplitude_vector));
-            text(text_location_x, text_location_y, equation_str, 'FontSize', 12, 'Color', 'k');
             hold off;
 
             % Save the plot as an image file with driving frequency included in the filename
